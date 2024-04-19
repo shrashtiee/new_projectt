5856a7d321757f58909d80b3033cbaca5dc30fb7
shrashtiee <shrashti391@gmail.com>
2024-04-19 16:10:05 +0530

    Added interceptor for making API Call

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,19 +2,51 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import HomeView from "./view";
+import axios from 'axios';
 
 const HomeScreen = (props) => {
     const { navigation } = props;
     const [countryData, setCountryData] = useState([]);
     const [loading, setLoading] = useState(false);
 
+    const api = axios.create({
+        baseURL: 'https://restcountries.com/v3.1/',
+    });
+
+    api.interceptors.request.use(
+        (config) => {
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
+    api.interceptors.response.use(
+        (response) => {
+            return response.data;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
     const handleFetch = async () => {
         setLoading(true)
-        const url = `https://restcountries.com/v3.1/all`;
-        const res = await fetch(url);
-        const data = await res.json();
-        setLoading(false)
-        setCountryData(data);
+        api.get('/all')
+            .then((response) => {
+                setLoading(false)
+                setCountryData(response);
+            })
+            .catch((error) => {
+                console.error('Error fetching countries:', error);
+            });
+
+        // const url = `https://restcountries.com/v3.1/all`;
+        // const res = await fetch(url);
+        // const data = await res.json();
+        // setLoading(false)
+        // setCountryData(data);
     }
 
     const handleItem = (item) => {
